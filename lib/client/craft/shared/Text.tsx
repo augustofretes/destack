653c58ded87e0f2768b82818e14bf113c8f48e4d
augustofretes@gmail.com <augustofretes>
2023-04-12 18:29:54 -0300

    Set default to black

--- lib/client/craft/shared/Text.tsx
+++ lib/client/craft/shared/Text.tsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
-import { useNode, useEditor } from '@craftjs/core';
+import React, { useState } from 'react'
+import { useNode, useEditor } from '@craftjs/core'
 
 interface TextProps {
-  id: string;
-  className: string;
-  key: string;
-  text: string;
-  color: string; // Add color property
+  id: string
+  className: string
+  key: string
+  text: string
+  color: string // Add color property
 }
 
 interface TextInterface extends React.FC<TextProps> {
-  craft: object;
+  craft: object
 }
 
 const Text: TextInterface = (props) => {
-  const { node, connectors, actions } = useNode((node) => ({ node }));
-  const { enabled } = useEditor((state) => ({ enabled: state.options.enabled }));
-  const [text] = useState(node.data.props[props.id]?.text ?? props.text);
-  const [color] = useState(node.data.props[props.id]?.color ?? props.color); // Add color state
+  const { node, connectors, actions } = useNode((node) => ({ node }))
+  const { enabled } = useEditor((state) => ({ enabled: state.options.enabled }))
+  const [text] = useState(node.data.props[props.id]?.text ?? props.text)
+  const [color] = useState(node.data.props[props.id]?.color ?? props.color) // Add color state
   const onChange = (e: any) => {
     actions.setProp((prop: any) => {
-      if (!prop[props.id]) prop[props.id] = {};
-      prop[props.id].text = e.target.innerText;
-    }, 500);
-  };
+      if (!prop[props.id]) prop[props.id] = {}
+      prop[props.id].text = e.target.innerText
+    }, 500)
+  }
 
   const onClick = (e: any) => {
     if (enabled) {
-      e.preventDefault();
-      e.stopPropagation();
+      e.preventDefault()
+      e.stopPropagation()
     }
-  };
+  }
 
   return enabled ? (
     <span
@@ -49,16 +49,16 @@ const Text: TextInterface = (props) => {
       {/* // style={{ ...props }} */}
       {text}
     </span>
-  );
-};
+  )
+}
 
-export { Text };
+export { Text }
 
 Text.craft = {
   displayName: 'Text',
   props: {
     text: '',
-    color: '#000000', // Add default color
+    color: '#000000', // Black, default color
   },
   related: {},
-};
+}
